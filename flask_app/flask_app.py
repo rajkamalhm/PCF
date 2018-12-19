47e226bf9c08fbf904fd7fdbf6246e5eca2c4ac4
rajkamalhm <rajkamalhm@gmail.com>
2018-12-19 19:09:36 +0530

    Update flask_app.py

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def myfunc():
-    return "Hey World with one instance!!!\n"
+    return "Hey World with two instance!!!\n"
 
 if __name__ == "__main__":
     app.run(port=8080, host='0.0.0.0')
