88f2f0e4e64274b035223bbe1b26e448eee3f4a5
rajkamalhm <rajkamalhm@gmail.com>
2018-12-19 18:57:08 +0530

    Update flask_app.py

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def myfunc():
-    return "Hey hello new World!!!"
+    return "Hey hello new World!!!\n"
 
 if __name__ == "__main__":
     app.run(port=8080, host='0.0.0.0')
